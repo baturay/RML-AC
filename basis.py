2e97b06a2542acfd114e982c3b87e1bdf183ba2c
Baturay Akaslan <baturay.akaslan@tufts.edu>
2012-02-10 02:03:17 -0500

    Random indentation error at basis.py Made a loop which inserts constraints 10 every iteration, does an EM run and calculates the information with NMI.

--- basis.py
+++ basis.py
@@ -12,16 +12,18 @@ class cData:
       self.means = []
       self.stddevs = []
       self.classes = []
-      self.cons = []
+      self.cons = [] #constraints so far
       self.parseCsv(filename)
+      self.poscons = [(i,j) for i in range(len(self.data)) for j in range(len(self.data))] #possible constraints
       self.zvalues()
-      self.poscons = [(i,j) for i in range(len(self.data)) for j in range(len(self.data))]
+	  
    def addDatum(self, values, index):
       new_datum = datum()
       new_datum.index = index #Every data's id is its row number
       new_datum.cl = int(values[0])
       new_datum.values = [float(x) for x in values[1:]]
       self.data.append(new_datum)
+	  
    def mean(self):
       sum = 0
       attCount = len(self.data[0].values)
@@ -30,8 +32,8 @@ class cData:
             sum+=i.values[j] 
          self.means.append(sum/len(self.data))
          sum = 0
-   def makeConst(self,numC):
-      constraints = []
+		 
+   def makeCons(self,numC):
       for i in range(numC):
          pair = self.poscons.pop(random.randint(0,len(self.poscons)-1))
          link = 0
@@ -41,6 +43,7 @@ class cData:
             link = -1 
          pair += (link,)
          self.cons.append(pair)
+		 
    def stddev(self):
       attCount = len(self.data[0].values)
       for j in range(0,attCount):
@@ -68,16 +71,14 @@ if __name__ == "__main__":
       sys.exit(1)
    numClusters = 3
    m = cData(sys.argv[1])
-   m.makeConst(10)
-   print m.cons
-   #iteration = EM(m)
-   #iteration.EM(numClusters)
-   # for i in m.data:
-      # print  str(i.index) + " " + str(i.values) + " " + i.cl
-   A = matrix([ [ random.random() for i in range(numClusters) ] for j in range (len(m.data)) ])
-   Estimated = np.ravel(A.argmax(1).T)
-   Real = array([ i.cl-1 for i in m.data])
-   print nmi(Estimated,Real)
+   for i in range(1000):
+      m.makeCons(10)
+      print m.cons
+      M= EM(m)
+      M.EM(numClusters)
+      Estimated = np.ravel(M.mGammas.argmax(1).T)
+      Real = array([ i.cl for i in m.data])
+      print nmi(Estimated,Real)
 
 
    
