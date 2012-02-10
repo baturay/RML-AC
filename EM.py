5046064d52bf5bb8dbe3ff64a85777e14049fa70
Eli Brown <ebrown@cs.tufts.edu>
2012-02-09 22:18:42 -0500

    ok, hopefully I'm right that it's working this time sorry about that

--- EM.py
+++ EM.py
@@ -4,9 +4,8 @@ from numpy import double
 from numpy import matrix
 from numpy import array
 import scipy.spatial as sp_s
-#from numpy import *
-#from scipy import *
-from basis import *
+#from basis import cData
+import random
 
 # this module contains a class that performs EM
 # clustering.  The input data is in the 'machine' format
@@ -25,6 +24,8 @@ class EM:
 
         self.bPPC = True # use PPC
         self.mCij = []
+
+        self.mGammas = [] # matrix prob of instance i in cluster j
 
     # returns true if we have reached convergence criteria
     def convergence(self):
@@ -47,9 +48,10 @@ class EM:
     # the P_l and the Sigma_l
     def clusterMembership(self, nData, nDataDim, lCenters, lPl, lSig, bPPC):
         coef = 1/(double(2*np.pi)**(nDataDim/double(2)))
+        print "lsig: " , lSig[0].shape
         sigCoefs = [ 1/np.sqrt(np.linalg.det(sig)) for sig in lSig ]
 
-        print lPl
+        print "lpl: " , lPl
 
         ppc_lambda = 2
         # compute single gamma value
@@ -88,16 +90,17 @@ class EM:
             if len(self.lInitialCenters) > numCenters:
                 self.lInitialCenters = self.lInitialCenters[:numCenters]
                 print "NOTE: given list of initial centers is too long, truncating"
-            elif len(self.lInitialCenters < numCenters):
+            elif len(self.lInitialCenters) < numCenters:
                 print "ERROR: provided too few initial centers"
                 sys.exit(1)
         else:  # pick centers from data
             self.lInitialCenters = random.sample(self.mData.data, numCenters)
+            self.lInitialCenters = [ c.values for c in self.lInitialCenters ]
 
         # initialization
         nDataDim = len(self.mData.data[0].values)
         nData = len(self.mData.data)
-        self.lCenters = [ c.values for c in self.lInitialCenters ]
+        self.lCenters = self.lInitialCenters[:]
         lPl = [ double(1)/len(self.lCenters) for l in range(len(self.lCenters)) ]
         lSig = [ np.eye(nDataDim) for i in range(len(self.lCenters)) ]
         lXi = [ self.mData.data[i].values for i in range(nData) ]
@@ -111,40 +114,30 @@ class EM:
             # estimate cluster membership
             # gets 2D array of Gamma_i,j
             # the prob that isntance i is in cluster j
-            aGammas = self.clusterMembership(nData, nDataDim, self.lCenters, lPl, lSig, self.bPPC)
+            self.mGammas = self.clusterMembership(nData, nDataDim, self.lCenters, lPl, lSig, self.bPPC)
             
             # recalculate parameters
-            lNl = [ np.sum(aGammas[:,l]) for l in range(len(self.lCenters)) ]
+            lNl = [ np.sum(self.mGammas[:,l]) for l in range(len(self.lCenters)) ]
             lPl = [ lNl[l] / nData for l in range(len(self.lCenters)) ]
             self.lCenters = [ np.multiply(1/lNl[l],
                                           reduce(lambda x,y: x + y,
-                                                 [ np.multiply(aGammas[i,l], lXi[i])
+                                                 [ np.multiply(self.mGammas[i,l], lXi[i])
                                                    for i in range(nData) ],
                                                  0 ) )
                               for l in range(len(self.lCenters)) ]
             print "new centers: "
             print self.lCenters
             
-            x = array(self.mData.data[0].values)
-            m = array(self.lCenters[0])
-            #printDim(x, "x")
-            #printDim(m, "m")
-            aXMuDiff = [ [ matrix(array(self.mData.data[i].values) - array(self.lCenters[l]))
+            aXMuDiff = [ [ matrix(array(lXi[i]) - array(self.lCenters[l]))
                            for i in range(nData) ]
                          for l in range(len(self.lCenters)) ]
-            #print "axmudif", np.size(aXMuDiff,0), np.size(aXMuDiff,1)
-            #print "axmudif elem", np.size(aXMuDiff[0][0],0), np.size(aXMuDiff[0][0],1)
-            xm = matrix(x - m)
-            #printDims(xm, "x-m")
-            xmp = xm.T * xm
-            #printDims(xmp, "xmp")
 
-            #printDims(aGammas[0,0], "aGammas elem")
-            #print "a gamma: ", aGammas[0,0]
-            
-            lSig = [ 1/lNl[l] *
-                     sum( [ np.multiply(aGammas[i,l], aXMuDiff[l][i].T * aXMuDiff[l][i])
-                            for i in range(nData) ] )
+            lSig = [ np.multiply(1/lNl[l],
+                                 reduce(lambda x,y: x+ y,
+                                        [ np.multiply(self.mGammas[i,l],
+                                                      aXMuDiff[l][i].T *
+                                                      aXMuDiff[l][i]) 
+                                          for i in range(nData) ] ) )
                      for l in range(len(self.lCenters)) ]
 
             iters += 1
@@ -156,3 +149,11 @@ def printDims(v, textv):
 def printDim(v, textv):
     print "dim ", textv, np.size(v,0)
 
+
+
+if __name__ == "__main__":
+    import basis
+    D = basis.cData("data/winenorm3_pyre.txt")
+    M = EM(D)
+    M.EM(3)
+    
