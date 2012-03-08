8e0aa281ee262d997bc7f11363e49b0b3a00bcdf
Baturay Akaslan <baturay.akaslan@tufts.edu>
2012-03-08 17:32:52 -0500

    Please run python datafile 3 metric to run. I haven't implemented file/random so far.

--- basis.py
+++ basis.py
@@ -9,40 +9,37 @@ class datum:
 class cData:
    def __init__(self,filename):
       self.data = []
-      self.means = []
-      self.stddevs = []
-      self.classes = dict()
+      self.classlist = dict()
       self.cons = [] #constraints so far
       self.parseCsv(filename)
       self.poscons = [] #possible constraints
       self.consfile = 0
-      self.zvalues()
-      self.real = array([self.classes[i.cl] for i in self.data])
+      self.classes = array([ self.classlist[i.cl] for i in self.data])
+      self.constype = 0
+      self.random = 0
+      self.startpoins = []
    def addDatum(self, values, index):
       new_datum = datum()
       new_datum.index = index #Every data's id is its row number
       new_datum.cl = values[0]
-      if(not (new_datum.cl in self.classes)):
-         self.classes[new_datum.cl] = len(self.classes)
+      #Classlist keeps all possible classes.
+      if(not (new_datum.cl in self.classlist)):
+         self.classlist[new_datum.cl] = len(self.classlist)
       new_datum.values = [float(x) for x in values[1:]]
       self.data.append(new_datum)
-      
-   def mean(self):
-      sum = 0
-      attCount = len(self.data[0].values)
-      for j in range(0,attCount):
-         for i in self.data:
-            sum+=i.values[j] 
-         self.means.append(sum/len(self.data))
-         sum = 0
-         
+
    def makeConst(self,numC):
-      cons = []
+      if self.constype == 2:
+         return pairCons(numC)
+      elif self.constype == 3:
+         return tripCons(numC)
+      else:
+         return []
+         
+   def pairCons(self,numC):
       for i in range(numC):
-         if(len(self.poscons)==1):
-            pair = self.poscons.pop()
-         else:
-            pair = self.poscons.pop(random.randint(0,len(self.poscons)-1))
+         pair = random.choice(self.poscons)
+         self.poscons.remove(pair)
          if(not self.consfile):
             link = 0
             if(self.data[pair[0]].cl==self.data[pair[1]].cl):
@@ -53,22 +50,7 @@ class cData:
          self.cons.append(pair)
          cons.append(pair)
       return cons
-   def stddev(self):
-      attCount = len(self.data[0].values)
-      for j in range(0,attCount):
-         sd = 0
-         for i in self.data:
-            sd += pow(i.values[j]-self.means[j],2)
-         sd /= len(self.data)-1
-         self.stddevs.append(pow(sd,0.5)) #Following the formula of standard deviation        
-   def zvalues(self):
-      self.mean()
-      self.stddev()
-      attCount = len(self.data[0].values)
-      for i in self.data:
-         for j in range(0,attCount):
-            i.values[j]=(i.values[j]-self.means[j])/self.stddevs[j]
-            
+                 
    def parseCsv(self,filename):
       with open(filename,"r") as fin:
          lines = fin.readlines()
@@ -83,7 +65,51 @@ class cData:
      for i in range(0, len(lines)):        
         values = lines[i].rstrip().split(",")
         self.poscons.append(array([ int(v) for v in values] ))
-   
+        
+   def tripCons(self,gammadiffs,k):
+      constraints = []
+      link = 0
+     
+      for i in range(k):
+         if(self.random):
+            cons = choice(gammadiffs)
+            gammadiffs.remove(cons)
+         else:
+            cons = gammadiffs.pop(0)
+         class1 = filter(lambda x: x[1] == cons[1],gammadiffs)
+         class2 = filter(lambda x: x[1] == cons[2],gammadiffs)
+         class1 = class1[int(np.floor(0.8*len(class1))):]
+         class2 = class2[int(np.floor(0.8*len(class2))):]
+
+         if len(class1)>0 and (m.data[cons[3]].cl == str(m.data[class1[-1][3]].cl)):
+            link = 1
+         elif len(class2)>0 and (m.data[cons[3]].cl == str(m.data[class2[-1][3]].cl)):
+            link = -1
+       
+         if(link != 0):
+            for i in class1:
+               constraints.append((cons[3],i[3],link))
+            for i in class2:
+               constraints.append((cons[3],i[3],-1*link))
+      return constraints
+      
+   def findDiffs(self,gammas):
+     gammadiffs = []
+     maxindices = np.ravel(gammas.argmax(1).T)
+     gammas = gammas.tolist()
+     for i in range(len(gammas)):
+         secondprob= -1
+         firstindex = maxindices[i]
+         firstprob = gammas[i][firstindex]
+         for j in range(len(gammas[i])):
+            if (gammas[i][j] > secondprob) and (j!=firstindex):
+               secondindex = j
+               secondprob = gammas[i][j]
+         metric = (firstprob-secondprob)/(firstprob+secondprob)
+         gammadiffs.append([metric,firstindex,secondindex,i])
+     gammadiffs = sorted(gammadiffs,key=lambda x:x[0])
+     return gammadiffs
+     
    def emRestarts(self,k):
       centers = []
       maxEM = EM(self)
@@ -91,39 +117,70 @@ class cData:
       for i in range(k):
          iteration = EM(self)
          nmi = self.evaluateEM(iteration)
-         print nmi
          if maxNMI < nmi:
             maxNMI = nmi
             maxEM = iteration
       return maxEM
       
    def evaluateEM(self,em):
-      em.EM(len(m.classes))
-      estimated = np.ravel(em.mGammas.argmax(1).T)
-      return nmi(estimated,self.real)
-
-if __name__ == "__main__":
-   if len(sys.argv) > 3:
-      print("Error - usage is " + sys.argv[0] + " <data_file>")
-      sys.exit(1)
+      em.EM(len(m.classlist))
+      Estimated = np.ravel(em.mGammas.argmax(1).T)
+      return nmi(Estimated,self.classes)
       
-  
+   def emInitialPoints(self,file):
+      emstart = EM(self)
+      with open(filename,"r") as fin:
+         lines = fin.readlines()
+      for i in range(1, len(lines)):
+         values = lines[i].rstrip().split(",")
+         self.startpoints.append(array([ float(v) for v in values] ))
+      return emstart
+if __name__ == "__main__":
+   if len(sys.argv) != 4 and len(sys.argv) != 5:
+      print("Error - usage is " + sys.argv[0] + " <data_file> <constrainttype> <constraintsource> <startingdatapoints>")
+      sys.exit(1)   
    m = cData(sys.argv[1])
-   if len(sys.argv) == 2:
-      m.poscons = [(i,j) for i in range(len(m.data)) for j in range(len(m.data))]
+   
+   if sys.argv[2] == "3":
+      m.constype = 3
+      if sys.argv[3] == "random":
+         m.random = 1
+         m.poscons = m.data[:]
+      elif sys.argv[3] == "metric":
+         m.poscons = m.data[:]
+      else:
+         parseConstraints(sys.argv[3])
+   elif sys.argv[2] == "2":
+      m.constype = 2
+      if sys.argv[3] == "random" or sys.argv[3] == "metric":
+         m.poscons = [(i,j) for i in range(len(m.data)) for j in range(len(m.data))]
+      else:
+         m.parseConstraints(sys.argv[3]) 
    else:
-      m.parseConstraints(sys.argv[2]) 
-   iteration = m.emRestarts(10)
+      m.constype = 1
+   
+   
+   if len(sys.argv)>4:
+      iteration = m.emInitial(file)
+      iteration.lInitialCenters = m.startpoints.pop(0)      
+   else:
+      iteration = m.emRestarts(9)
    iteration.bPPC = True
    prevCons = 0
-   for numCons in [0,25,50,100,200,500,1000,2000,5000,10000]:
-   #for numCons in range(0,len(m.poscons)):
-       cons=m.makeConst(numCons-prevCons)
-       prevCons = numCons
-       for i in cons:
-           iteration.mCij[i[0]][i[1]] = i[2]
-           iteration.mCij[i[1]][i[0]] = i[2]
-       nmiresult = m.evaluateEM(iteration)
-       print numCons, ",", nmiresult
-       if(nmiresult == 1):
+
+   totalcons = 0
+   for numCons in range(5,len(m.data),5): 
+      gammadiffs = m.findDiffs(iteration.mGammas)
+      #The boolean at the end of next line is True for random
+      #False for metric constraints.
+      cons = m.tripCons(gammadiffs,numCons-prevCons)
+      prevCons = numCons
+      totalcons += len(cons)
+      for i in cons:
+        iteration.mCij[i[0]][i[1]] = i[2]
+        iteration.mCij[i[1]][i[0]] = i[2]
+      
+      nmiresult = m.evaluateEM(iteration)
+      print numCons, ",", nmiresult, ",", totalcons
+      if(nmiresult == 1 or len(m.data)==numCons):
          break
