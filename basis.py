4e7273340d4ff2f42ee7aef98ebe6d1bf9d6c866
Baturay Akaslan <baturay.akaslan@tufts.edu>
2012-04-05 21:28:53 -0400

    Some structural changes. Some changes to implement asking the users for starting points.

--- basis.py
+++ basis.py
@@ -4,9 +4,17 @@ import pickle
 from EM import *
 from NMI import *
 class datum:
-   values = []
-   name = ""
-   cl = ""
+   def __init__(self):
+		self.values = []
+		self.name = ""
+		self.cl = ""
+class emcluster:
+   def __init__(self):
+      self.center = []
+      self.index = 0
+      self.outerpoints = []
+      self.midpoints = []
+      self.points = []
 class cData:
    def __init__(self,filename):
       self.data = []
@@ -90,11 +98,10 @@ class cData:
    def tripCons(self,mGammas,k):
       gammadiffs = m.findDiffs(mGammas)
       constraints = []
-      link = 0
-     
+      link = 0   
       for i in range(k):
          if(not self.consselect):
-            cons = choice(gammadiffs)
+            cons = R.choice(gammadiffs)
             gammadiffs.remove(cons)
          else:
             cons = gammadiffs.pop(0)
@@ -102,12 +109,11 @@ class cData:
          class2 = sorted(filter(lambda x: x[1] == cons[2],gammadiffs),key=lambda y: y[3])
          class1 = class1[int(np.floor(0.8*len(class1))):]
          class2 = class2[int(np.floor(0.8*len(class2))):]
-
          if len(class1)>0 and (m.data[cons[4]].cl == str(m.data[class1[-1][4]].cl)):
             link = 1
          elif len(class2)>0 and (m.data[cons[4]].cl == str(m.data[class2[-1][4]].cl)):
             link = -1
-       
+         print link
          if(link != 0):
             for i in class1:
                constraints.append((cons[4],i[4],link))
@@ -119,19 +125,64 @@ class cData:
      gammadiffs = []
      maxindices = np.ravel(gammas.argmax(1).T)
      gammas = gammas.tolist()
-     for i in range(len(gammas)):
-         secondprob= -1
+     for i,gamma in enumerate(gammas):
+         secondprob= -inf
          firstindex = maxindices[i]
-         firstprob = gammas[i][firstindex]
-         for j in range(len(gammas[i])):
-            if (gammas[i][j] > secondprob) and (j!=firstindex):
+         firstprob = gamma[firstindex]
+         for j in range(len(gamma)):
+            if (gamma[j] > secondprob) and (j!=firstindex):
                secondindex = j
-               secondprob = gammas[i][j]
+               secondprob = gamma[j]
          metric = (firstprob-secondprob)/(firstprob+secondprob)
          gammadiffs.append([metric,firstindex,secondindex,firstprob,i])
      gammadiffs = sorted(gammadiffs,key=lambda x:x[0])
      return gammadiffs
      
+   def findDistances(self,sources,cluster):
+      distances = []   
+      for i in cluster:
+         distance = 0
+         point = self.data[i[0]]
+         for s in sources:
+            for index,value in enumerate(point.values):
+               distance += (value - s[index])**2
+         distances.append([i,distance])
+  
+      return distances    
+      
+   def repPoints(self,EM):
+      maxindices = np.ravel(EM.mLikelihood_il.argmax(1).T)
+      ivpairs = []
+      for i,value in enumerate(maxindices):
+         ivpairs.append([i,value])
+      clusters = []
+      for i,center in enumerate(EM.lCenters):
+         c = emcluster()
+         c.points = filter(lambda x: x[1]==i,ivpairs)
+         c.center = center.tolist()
+         clusters.append(c)
+		 
+      for cl in clusters:
+         cdist = sorted(self.findDistances([cl.center],cl.points),key = lambda x : x[1])
+         cl.center = self.data[cdist[0][0][0]].values
+         cl.points.remove(cdist[0][0])        
+         cl.outerpoints.append(self.data[cdist[-1][0][0]].values)
+         cl.points.remove(cdist[-1][0])
+         
+         for i in range(3):
+            odist = sorted(self.findDistances(cl.outerpoints,cl.points),key = lambda x : x[1])
+            cl.outerpoints.append(self.data[odist[-1][0][0]].values)
+            cl.points.remove(odist[-1][0])
+         for o in cl.outerpoints:
+            midpoint = []
+            for index,value in enumerate(o):
+               midpoint.append((value+cl.center[index])/2)
+            mdist = sorted(self.findDistances([midpoint],cl.points),key = lambda x : x[1])
+            cl.midpoints.append(midpoint)
+         print cl.midpoints
+            # mdist = sorted(self.findDistances([i],cl.points),key = lambda x : x[1])
+            
+         
    def emRestarts(self,k):
       centers = []
       maxEM = EM(self)
@@ -150,47 +201,46 @@ class cData:
       Estimated = np.ravel(em.mGammas.argmax(1).T)
       return nmi(Estimated,self.classes)
       
-   # def emInitialPoints(self,file):
-      # emstart = EM(self)
-      # with open(filename,"r") as fin:
-         # lines = fin.readlines()
-      # for i in range(1, len(lines)):
-         # values = lines[i].rstrip().split(",")
-         # self.startpoints.append(array([ float(v) for v in values] ))
-      # return emstart
+   def parseCommandLine (self,argv):
+      if len(argv) != 2 and len(argv) != 3:
+         print("Error - usage is " + argv[0] + " <data_file> <startingdatapoints>")
+         sys.exit(1)   
+        
+      if len(argv)>2:
+         EmAlg = EM(self)
+         f = open(argv[2],"r")
+         centers = pickle.load(f)
+         EmAlg.lInitialCenters = centers
+         EmAlg.EM(len(self.classlist))
+      else:
+         EmAlg = self.emRestarts(5)
+         f = open("centers.pickle","w")
+         l = EmAlg.lCenters
+         pickle.dump(l,f)
+      self.repPoints(EmAlg)
+      return EmAlg
       
 if __name__ == "__main__":
-   if len(sys.argv) != 4 and len(sys.argv) != 5:
-      print("Error - usage is " + sys.argv[0] + " <data_file> <constrainttype> <constraintsource> <startingdatapoints>")
-      sys.exit(1)   
    m = cData(sys.argv[1])
-   m.setType(sys.argv[2],sys.argv[3])
-   if len(sys.argv)>4:
-      EmAlg = EM(m)
-      f = open(sys.argv[4],"r")
-      centers = pickle.load(f)
-      EmAlg.lInitialCenters = centers
-      EmAlg.EM(len(m.classlist))
-   else:
-      EmAlg = m.emRestarts(10)
-      f = open("centers.pickle","w")
-      l = EmAlg.lCenters
-      pickle.dump(l,f)
+   m.setType("3","metric")
+   EmAlg = m.parseCommandLine(sys.argv) 
    EmAlg.bPPC = True
-   prevCons = 0
    
+   prevCons = 0
    totalcons = 0
-   for numCons in range(1,len(m.data)/4,1):     
-      #The boolean at the end of next line is True for random
-      #False for metric constraints.
+   initial = EmAlg.lInitialCenters
+   nmiResult = m.evaluateEM(EmAlg)
+   print "Initial nmi: ",nmiResult
+   for numCons in range(1,len(m.data),1):     
       cons = m.tripCons(EmAlg.mGammas,numCons-prevCons)
       prevCons = numCons
       totalcons += len(cons)
       for i in cons:
         EmAlg.mCij[i[0]][i[1]] = i[2]
-        EmAlg.mCij[i[1]][i[0]] = i[2]
-      
+        EmAlg.mCij[i[1]][i[0]] = i[2]    
       nmiresult = m.evaluateEM(EmAlg)
-      print numCons, ",", nmiresult, ",", totalcons
+      print numCons, ",", nmiresult, ",", totalcons 
       if(nmiresult == 1 or len(m.data)==numCons):
          break
+   print numCons, ",", nmiresult, ",", totalcons      
+         
